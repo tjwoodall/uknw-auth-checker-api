39e40959ac5810bea5121d947da86a00780d0c82
Alessandro Cinque <alessandro5cinque@gmail.com>
2024-09-24 17:08:28 +0100

    NP-335 routine update (#82)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatestplus"      %% "scalacheck-1-18"        % "3.2.19.0"       % Test,
     "io.github.wolfendale"   %% "scalacheck-gen-regexp"  % "1.1.0"          % Test,
-    "org.scalacheck"         %% "scalacheck"             % "1.18.0"         % Test,
+    "org.scalacheck"         %% "scalacheck"             % "1.18.1"         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1"          % Test
   )
 
