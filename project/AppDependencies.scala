8c9c9544265d21cef9056bad0950bba13854147e
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2025-02-05 13:46:07 +0000

    NP-Packages: Updated packages (#92)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
+  private val bootstrapVersion = "9.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "org.typelevel" %% "cats-core"                 % "2.12.0"
+    "org.typelevel" %% "cats-core"                 % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
