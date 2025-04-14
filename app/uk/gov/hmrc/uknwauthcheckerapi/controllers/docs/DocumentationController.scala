04b492abca8b5a196d76a06fdc8dbf32db2c5309
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2025-04-14 15:38:54 +0100

    Revert "Revert "NP-Routes: Moved definition.routes into it's own package to f…" (#107)
    This reverts commit f82eb8bc291f4270f420e02c346412b508310c29.

--- app/uk/gov/hmrc/uknwauthcheckerapi/controllers/docs/DocumentationController.scala
+++ app/uk/gov/hmrc/uknwauthcheckerapi/controllers/docs/DocumentationController.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.uknwauthcheckerapi.controllers
+package uk.gov.hmrc.uknwauthcheckerapi.controllers.docs
 
 import javax.inject.{Inject, Singleton}
 
